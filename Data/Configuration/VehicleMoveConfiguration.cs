42b62b6e7cdde7c1dfce0a740c4a494b8546ad68
batuwan <batuhankaraman@gmail.com>
2020-11-30 18:07:12 +0300

    add changes to vehicleMoveConfig

--- Data/Configuration/VehicleMoveConfiguration.cs
+++ Data/Configuration/VehicleMoveConfiguration.cs
@@ -16,6 +16,10 @@ namespace VehicleTracker.Data.Configuration
             builder.Property(x => x.Id).UseIdentityColumn();
 
             //TODO
+            builder.Property(x => x.Geom).IsRequired();
+            builder.Property(x => x.VehicleId).IsRequired();
+            builder.Property(x => x.Date_).IsRequired();
+            
         }
     }
 }
