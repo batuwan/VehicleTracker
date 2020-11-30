fa301908e6f143177976c225e027681cf9b651da
Piton <Piton@PITON>
2020-11-30 17:33:43 +0300

    add service interfaces

---
+++ Core/Service/IVehicleMoveService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
+
+namespace VehicleTracker.Core.Service
+{
+    interface IVehicleMoveService : IService<VehicleMove>
+    {
+        //TODO: 
+    }
+}
