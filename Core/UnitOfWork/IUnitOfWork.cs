c5b9cd5e1566455fd50644d7cf37f9521404413e
Piton <Piton@PITON>
2020-11-30 17:45:26 +0300

    add IUnitOfWork, AppDbContext

---
+++ Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.IRepository;
+
+namespace VehicleTracker.Core.UnitOfWork
+{
+    interface IUnitOfWork
+    {
+        IVehicleRepository Vehicles { get; }
+        IZoneRepository Zones { get; }
+        IVehicleMoveRepository VehicleMoves { get; }
+        IZoneRecordRepository ZoneRecords { get; }
+
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
+
+namespace VehicleTracker.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleMove> VehicleMoves { get; set; }
+        public DbSet<Zone> Zones { get; set; }
+        public DbSet<ZoneRecord> ZoneRecords { get; set; }
+    }
+}
