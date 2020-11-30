292ff260fbf7ba695a532c757ca1a11d9d18a993
Piton <Piton@PITON>
2020-11-30 17:54:47 +0300

    add VehicleConfiguration, ZoneConfiguration

---
+++ Data/Configuration/VehicleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
+
+namespace VehicleTracker.Data.Configuration
+{
+    public class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+    {
+        public void Configure(EntityTypeBuilder<Vehicle> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(x => x.Plate).IsRequired();
+        }
+    }
+}
---
+++ Data/Configuration/ZoneConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
+
+namespace VehicleTracker.Data.Configuration
+{
+    public class ZoneConfiguration : IEntityTypeConfiguration<Zone>
+    {
+        public void Configure(EntityTypeBuilder<Zone> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(x => x.Name);
+            builder.Property(x => x.Geom).IsRequired();
+
+        }
+    }
+}
