0c59ef84cb993045fef2691ea71f1d51cb131917
Piton <Piton@PITON>
2020-11-30 17:58:00 +0300

    add VehicleMoveConfig, ZoneRecordConfig classes (not Implemented)

---
+++ Data/Configuration/VehicleMoveConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
+
+namespace VehicleTracker.Data.Configuration
+{
+    public class VehicleMoveConfiguration : IEntityTypeConfiguration<VehicleMove>
+    {
+        public void Configure(EntityTypeBuilder<VehicleMove> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Configuration/ZoneRecordConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
+
+namespace VehicleTracker.Data.Configuration
+{
+    public class ZoneRecordConfiguration : IEntityTypeConfiguration<ZoneRecord>
+    {
+        public void Configure(EntityTypeBuilder<ZoneRecord> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
