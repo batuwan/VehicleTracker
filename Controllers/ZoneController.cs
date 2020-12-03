63836059c0f9b7fcc8393af758ad91ce1c6370a4
batuwan <batuhankaraman@gmail.com>
2020-12-03 16:05:14 +0300

    add endpoints to ZoneController, ZoneRecordController

--- Controllers/ZoneController.cs
+++ Controllers/ZoneController.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
 using VehicleTracker.Core.Service;
+using VehicleTracker.Core.UnitOfWork;
 using VehicleTracker.DTOs;
 
 namespace VehicleTracker.Controllers
@@ -16,9 +18,11 @@ namespace VehicleTracker.Controllers
     {
         private readonly IZoneService _zoneService;
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public ZoneController(IZoneService zoneService, IMapper mapper, IUnitOfWork unitOfWork )
 
-        public ZoneController(IZoneService zoneService, IMapper mapper )
         {
+            _unitOfWork = unitOfWork;
             _zoneService = zoneService;
             _mapper = mapper;
         }
@@ -30,6 +34,42 @@ namespace VehicleTracker.Controllers
             var zones = await _zoneService.GetAllAsync();
 
             return Ok(_mapper.Map<IEnumerable<ZoneDTO>>(zones));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var zone = await _zoneService.GetByIdAsync(id);
+            return Ok(_mapper.Map<ZoneDTO>(zone));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ZoneDTO zoneDTO)
+        {
+            var newZone = await _zoneService.AddAsync(_mapper.Map<Zone>(zoneDTO));
+
+            return Created(string.Empty, _mapper.Map<ZoneDTO>(newZone));
+        }
+
+
+
+        [HttpPut]
+        public IActionResult Update(ZoneDTO zoneDTO)
+
+        {
+            var zone = _zoneService.Update(_mapper.Map<Zone>(zoneDTO));
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var zone = _zoneService.GetByIdAsync(id).Result;
+            _zoneService.Remove(zone);
+            _unitOfWork.Commit();
+
+            return NoContent();
         }
     }
 }
--- Controllers/ZoneRecordController.cs
+++ Controllers/ZoneRecordController.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VehicleTracker.Core.Model;
 using VehicleTracker.Core.Service;
+using VehicleTracker.Core.UnitOfWork;
 using VehicleTracker.DTOs;
 
 namespace VehicleTracker.Controllers
@@ -17,11 +19,13 @@ namespace VehicleTracker.Controllers
 
         private readonly IZoneRecordService _zoneRecordService;
         private readonly IMapper _mapper;
-
-        public ZoneRecordController(IZoneRecordService zoneRecordService, IMapper mapper)
+        private readonly IUnitOfWork _unitOfWork;
+        public ZoneRecordController(IZoneRecordService zoneRecordService, IMapper mapper, IUnitOfWork unitOfWork)
         {
             _zoneRecordService = zoneRecordService;
             _mapper = mapper;
+            _unitOfWork = unitOfWork;
+
         }
 
         [HttpGet]
@@ -30,6 +34,39 @@ namespace VehicleTracker.Controllers
             var zoneRecords = await _zoneRecordService.GetAllAsync();
 
             return Ok(_mapper.Map<IEnumerable<ZoneRecordDTO>>(zoneRecords));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var zoneRecord = await _zoneRecordService.GetByIdAsync(id);
+            return Ok(_mapper.Map<ZoneRecordDTO>(zoneRecord));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ZoneRecordDTO zoneRecordDTO)
+        {
+            var newZoneRecord = await _zoneRecordService.AddAsync(_mapper.Map<ZoneRecord>(zoneRecordDTO));
+
+            return Created(string.Empty, _mapper.Map<ZoneRecordDTO>(newZoneRecord));
+        }
+
+        [HttpPut]
+        public IActionResult Update(ZoneRecordDTO zoneRecordDTO)
+
+        {
+            var zoneRecord = _zoneRecordService.Update(_mapper.Map<ZoneRecord>(zoneRecordDTO));
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var zoneRecord = _zoneRecordService.GetByIdAsync(id).Result;
+            _zoneRecordService.Remove(zoneRecord);
+            _unitOfWork.Commit();
+
+            return NoContent();
         }
     }
 }
